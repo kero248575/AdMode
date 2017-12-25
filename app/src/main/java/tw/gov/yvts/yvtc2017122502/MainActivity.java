872cfeca05908254409f9b173191c309a1cde288
Shaojung <shaojung@gmail.com>
2017-12-25 15:53:45 +0800

    Initial commit

---
+++ app/src/main/java/tw/gov/yvts/yvtc2017122502/MainActivity.java
@@ -0,0 +1,22 @@
+package tw.gov.yvts.yvtc2017122502;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-8454644485395965~7073145240");
+        
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
